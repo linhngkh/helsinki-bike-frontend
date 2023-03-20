37c270b1c4de6a7d625a7f0571827da8b5c61b73
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-03-20 14:32:58 +0200

    created router for frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,43 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Link,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
 import { Journeys } from "./components/Journeys";
+import LandingPage from "./components/LandingPage";
 import "./index.css";
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />}>
+        <Route index element={<LandingPage />} />
+        <Route path="/journeys" element={<Journeys />} />
+      </Route>
+    )
+  );
+
   return (
     <div>
-      <Journeys />
+      <RouterProvider router={router} />
     </div>
   );
 }
+
+const Root = () => {
+  return (
+    <>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/journeys">Journey</Link>
+      </div>
+      <div>
+        <Outlet />
+      </div>
+    </>
+  );
+};
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,9 @@ import { ReactQueryDevtools } from "react-query/devtools";
 
 const queryClient = new QueryClient();
 
+
+
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
