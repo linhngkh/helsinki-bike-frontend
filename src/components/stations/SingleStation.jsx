dc9294fedf2f4503d85b158fe601d0fcb23f653d
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-03-25 19:12:52 +0200

    rnder iframe on single station page

--- src/components/stations/SingleStation.jsx
+++ src/components/stations/SingleStation.jsx
@@ -1,57 +1,48 @@
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
-import { Map, Marker } from "pigeon-maps";
-import { osm } from "pigeon-maps/providers";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
-const SingleStation = () => {
+const SingleStation = (props) => {
   //create route parameters
   const { id } = useParams();
-  const [center, setCenter] = useState([50.879, 4.6997]);
-  const [zoom, setZoom] = useState(11);
+  const address = props.Osoite;
+  const city = props.Kaupunki;
 
   return (
     <Card
       sx={{
-        maxWidth: 400,
+        maxWidth: 700,
         display: "flex",
-        margin: "0 auto",
-        flexDirection: "column",
+        margin: "50px auto",
         alignItems: "center",
         justifyContent: "center",
-        minHeight: "100vh",
+        minHeight: "60vh",
       }}
     >
+      <CardContent style={{ width: "100%" }}>
+        <iframe
+          width="100%"
+          height="500"
+          style={{ border: "none" }}
+          src="https://maps.google.com/maps?hl=en&amp;q=${address}+${city}&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+        ></iframe>
+      </CardContent>
       <CardContent>
-        <Map
-          provider={osm}
-          height={500}
-          center={center}
-          zoom={zoom}
-          onBoundsChanged={({ center, zoom }) => {
-            setCenter(center);
-            setZoom(zoom);
-          }}
-        >
-          <Marker width={50} anchor={[50.879, 4.6997]} />
-        </Map>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
+        <Typography gutterBottom variant="h6" component="div">
+          Name
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
+          Address
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Total number of journeys starting from the station
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Total number of journeys ending at the station
         </Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
     </Card>
   );
 };
--- src/components/stations/Station.jsx
+++ src/components/stations/Station.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import TableHeader, { columns } from "./TableHeader";
 import DirectionsBikeIcon from "@mui/icons-material/DirectionsBike";
 import TipsAndUpdatesIcon from "@mui/icons-material/TipsAndUpdates";
