779186fa9aa1efdaee96106de211d5a74a570cd1
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-03-31 11:53:12 +0300

    adding cypress and writing tests

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,12 @@
+describe("website", () => {
+  it("user visit homepage and see hero video", () => {
+    // user visit homepage
+    cy.visit("http://localhost:5206/");
+    // user click to link journey navbar
+    // user click to link station navbar
+    // user click to button station
+    // user click to button journey
+    // user search for journey info
+    // user search for station info
+  });
+});
